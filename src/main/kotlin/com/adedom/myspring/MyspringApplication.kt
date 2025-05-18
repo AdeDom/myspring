8f5080fef567de7fe0c3b8ba6319203d4708b7c2
pathiphon <pathiphon.j@muze.co.th>
2025-05-18 12:03:29 +0700

    Initial commit.

---
+++ src/main/kotlin/com/adedom/myspring/MyspringApplication.kt
@@ -0,0 +1,11 @@
+package com.adedom.myspring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MyspringApplication
+
+fun main(args: Array<String>) {
+	runApplication<MyspringApplication>(*args)
+}
